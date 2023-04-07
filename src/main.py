2fa5d75e9d5ca5411831824ae557830fae05b194
Vadim Andreev <vadimcola@mail.ru>
2023-04-07 17:32:11 +0700

    add

--- src/main.py
+++ src/main.py
@@ -1,18 +1,31 @@
 import requests
 import json
 
-url = requests.get('https://api.hh.ru/vacancies')
-response_data = json.loads(url.text)
-job_title = response_data['items'][0]['name']
-salary_min = response_data['items'][0]['salary']['from']
-salary_max = response_data['items'][0]['salary']['to']
-employee_requirement = response_data['items'][0]['snippet']['requirement']
-id_vacancy = response_data['items'][0]['id']
-url_vacancy = f'https://hh.ru/vacancy/{id_vacancy}'
-
-print(url_vacancy)
+import os
 
 
+# url = requests.get('https://api.hh.ru/vacancies')
+# response_data = json.loads(url.text)
+# job_title = response_data['items'][0]['name']
+# salary_min = response_data['items'][0]['salary']['from']
+# salary_max = response_data['items'][0]['salary']['to']
+# employee_requirement = response_data['items'][0]['snippet']['requirement']
+# id_vacancy = response_data['items'][0]['id']
+# url_vacancy = f'https://hh.ru/vacancy/{id_vacancy}'
+#
+# print(url_vacancy)
 
 
+API_KEY = os.getenv('API-KEY')
+#
+# url = 'https://api.superjob.ru/2.0/vacancies'
+#
+# response = requests.get(url, API_KEY)
+# response_data = json.loads(response.text)
+#
+# print(response_data)
 
+
+response = requests.get('https://api.superjob.ru/2.0/vacancies')
+print(response.status_code)
+print(response.content)
