815c247ce84729da6c749aab32823d4e6e548354
Vadim Andreev <vadimcola@mail.ru>
2023-04-07 12:24:27 +0700

    add

--- src/main.py
+++ src/main.py
@@ -3,7 +3,13 @@ import json
 
 url = requests.get('https://api.hh.ru/vacancies')
 response_data = json.loads(url.text)
+job_title = response_data['items'][0]['name']
+salary_min = response_data['items'][0]['salary']['from']
+salary_max = response_data['items'][0]['salary']['to']
+employee_requirement = response_data['items'][0]['snippet']['requirement']
+id_vacancy = response_data['items'][0]['id']
+url_vacancy = f'https://hh.ru/vacancy/{id_vacancy}'
 
+print(url_vacancy)
 
-print(response_data)
 
