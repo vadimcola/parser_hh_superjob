361c1e57091e6c4952b533a1547d47159b17b804
Vadim Andreev <vadimcola@mail.ru>
2023-04-09 01:12:47 +0700

    no comment

--- src/main.py
+++ src/main.py
@@ -4,22 +4,22 @@ import json
 import os
 
 #
-# url = requests.get('https://api.hh.ru/vacancies')
-# response_data = json.loads(url.text)
+# response_url = requests.get('https://api.hh.ru/vacancies')
+# response_data = json.loads(response_url.text)
 # job_title = response_data['items'][0]['name']
 # salary_min = response_data['items'][0]['salary']['from']
 # salary_max = response_data['items'][0]['salary']['to']
 # employee_requirement = response_data['items'][0]['snippet']['requirement']
 # id_vacancy = response_data['items'][0]['id']
 # url_vacancy = f'https://hh.ru/vacancy/{id_vacancy}'
-
+#
 # print(response_data)
 
 
 API_KEY = os.environ.get('API-KEY')
-key = {'X-Api-App-Id': API_KEY}
-url = 'https://api.superjob.ru/2.0/vacancies'
-response = requests.get(url, headers=key)
+KEY = {'X-Api-App-Id': API_KEY}
+response_url = 'https://api.superjob.ru/2.0/vacancies'
+response = requests.get(response_url, headers=KEY)
 response_data = json.loads(response.text)
 url_vacancy = response_data['objects'][0]['link']
 job_title = response_data['objects'][0]['profession']
@@ -27,4 +27,4 @@ salary_min = response_data['objects'][0]['payment_from']
 salary_max = response_data['objects'][0]['payment_to']
 employee_requirement = response_data['objects'][0]['candidat']
 
-print(url_vacancy)
+print(response)
---
+++ src/uploading.py
@@ -0,0 +1,53 @@
+import json
+import os
+from abc import ABC, abstractmethod
+
+import requests
+
+
+class API(ABC):
+    @abstractmethod
+    def uploading(self):
+        pass
+
+
+class HeadHunterAPI(API, ABC):
+    def __init__(self):
+        self.params = None
+        self.vacancies = None
+        self.response_data = None
+        self.response_url = None
+
+    def uploading(self, *args):
+        self.params = {'text': args,
+                       'page': 1,
+                       'per_page': 100
+                       }
+        self.response_url = requests.get('https://api.hh.ru/vacancies', params=self.params)
+        self.response_data = json.loads(self.response_url.text)
+        self.vacancies = self.response_data['items']
+        return self.vacancies
+
+
+class SuperJobAPI(API, ABC):
+    API_KEY = os.environ.get('API-KEY')
+    KEY = {'X-Api-App-Id': API_KEY}
+
+    def __init__(self):
+        self.params = None
+        self.vacancies = None
+        self.response_data = None
+        self.response = None
+        self.response_url = None
+
+    def uploading(self, *args):
+        self.params = {'keywords': args}
+        self.response_url = 'https://api.superjob.ru/2.0/vacancies'
+        self.response = requests.get(self.response_url, headers=self.KEY, params=self.params)
+        self.response_data = json.loads(self.response.text)
+        self.vacancies = self.response_data['objects']
+        return self.vacancies
+
+
+url = HeadHunterAPI()
+print(url.uploading("Python"))
